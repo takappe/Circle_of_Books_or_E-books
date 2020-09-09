012567cdbbbf7f2ddd85a38148a494aa7373d00f
takappe <green.na.ore.ha.bz@gmail.com>
2020-09-09 17:52:33 +0900

    userのnicknameの保存

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,18 @@
 class BlogsController < ApplicationController
+  before_action :move_to_index, except: [:index, :show]
+
   def index
     @blogs = Blog.all
+  end
+
+  def new
+    @blog = Blog.all
+  end
+
+  private
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 end

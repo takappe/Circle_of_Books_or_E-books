6d962d5e7ef3c48a68764baf8565a5cd0c8cb44f
takappe <green.na.ore.ha.bz@gmail.com>
2020-11-05 16:41:32 +0900

    db修正

--- db/schema.rb
+++ db/schema.rb
@@ -12,16 +12,16 @@
 
 ActiveRecord::Schema.define(version: 2020_09_06_020612) do
 
-  create_table "blogs", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.text "blog"
-    t.bigint "user_id"
+    t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_blogs_on_user_id"
   end
 
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "users", force: :cascade do |t|
     t.string "nickname", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
